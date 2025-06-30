fe719426c0beec2af062afcd6fab94acc3878e39
slupjen <slupjen@wpg.com.ua>
2025-06-30 05:04:52 +0300

    Fix build with binary packages

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import logging
-from aiogram import Bot, Dispatcher, types, executor
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
